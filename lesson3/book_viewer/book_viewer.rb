237263415af116de1afb60029164e5cbd0b0e413
Anne Tiotuico <amtiotuico@gmail.com>
2021-08-18 01:27:41 +0000

    added a dynamic route for each chapter, a before filter, an in_paragraphs helper, and redirects

--- lesson3/book_viewer/book_viewer.rb
+++ lesson3/book_viewer/book_viewer.rb
@@ -2,21 +2,40 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+helpers do 
+  def in_paragraphs(text)
+    text.split("\n\n")
+  end
+end
+
+not_found do
+  redirect "/"
+end
+
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
   
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
-  @contents = File.readlines("data/toc.txt")
-  @chapter = File.read("data/chp1.txt")
-
+get "/chapters/:number" do
+  @number = params[:number].to_i
+  @chapter_title = @contents.at(@number - 1)
+  
+  redirect "/" unless (1..@contents.size).cover?(@number)
+  
+  
+  @title = "Chapter #{@number} - #{@chapter_title}"
+  @chapter = File.read("data/chp#{@number}.txt")
+  
   erb :chapter
 end
-# get "/:something" do |n|
-#   @title = n
-#   erb :home
-# end
+
+get "/show/:name" do
+  params[:name]
+end
