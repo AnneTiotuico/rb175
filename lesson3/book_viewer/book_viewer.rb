a87458555aa48f983b616a142e3d34be94b6a60b
Anne Tiotuico <amtiotuico@gmail.com>
2021-08-19 02:02:57 +0000

    added search form

--- lesson3/book_viewer/book_viewer.rb
+++ lesson3/book_viewer/book_viewer.rb
@@ -16,7 +16,6 @@ not_found do
   redirect "/"
 end
 
-
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
   
@@ -32,10 +31,39 @@ get "/chapters/:number" do
   
   @title = "Chapter #{@number} - #{@chapter_title}"
   @chapter = File.read("data/chp#{@number}.txt")
-  
+
   erb :chapter
 end
 
-get "/show/:name" do
-  params[:name]
+get "/search" do
+  @search_text = params[:query]
+  @results = ''
+  @found = {}
+  
+  if @search_text
+    @contents.each_with_index do |ch, idx|
+      @ch_text = File.read("data/chp#{idx + 1}.txt")
+      if @ch_text.include?(@search_text)
+        @found[ch] = idx
+      end
+    end
+    unless @found.empty?
+      @results = "<h2 class='content-subhead'>Results for '#{@search_text}'</h2>"
+    else
+      @results = "<p>Sorry, no matches were found.</p>"
+    end
+  end
+  
+  erb :search
 end
+
+=begin 
+if results are found, display "<h2>Results for 'truck'<h2>"
+and the chapter links
+else, display <p>"Sorry, no matches found"</p>
+
+if text is found in any chapters, return that chapter and a link to it as an unordered list
+iterate through each chapter and see if the text is matched in the chapter, if so return that chapter title
+
+
+=end
